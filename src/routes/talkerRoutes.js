76cf9e3af8b2e0f8b9fd462cd98105e8676c516b
jeferson-viana <jefersonv28@gmail.com>
2023-06-19 16:10:29 -0300

    Requisito 2 concluído

--- src/routes/talkerRoutes.js
+++ src/routes/talkerRoutes.js
@@ -9,6 +9,14 @@ talkerRoutes.get('/', async (req, res) => {
   return res.status(200).json(getAllTalkerManagers);
 });
 
+talkerRoutes.get('/:id', async (req, res) => {
+  const { id } = req.params;
+  const getAllTalkerManagers = await readFileTalker();
+  const getTalkerById = getAllTalkerManagers.find((talker) => talker.id === Number(id));
+  if (getTalkerById) return res.status(200).json(getTalkerById);
+  return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
+});
+
 // talkerRoutes.get('/', () => {
 
 // });
