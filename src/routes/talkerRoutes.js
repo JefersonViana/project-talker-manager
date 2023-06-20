0b075a9fe158baef5e7058612efb810de4b222fc
jeferson-viana <jefersonv28@gmail.com>
2023-06-20 20:12:41 -0300

    Requisito 10 concluído

--- src/routes/talkerRoutes.js
+++ src/routes/talkerRoutes.js
@@ -10,6 +10,9 @@ const {
   validateWatchAt,
   validateRate,
   validateRateQuery,
+  validateDateQuery,
+  validateAllQueries,
+  validateQueries,
 } = require('../validations/validateTalker');
 
 const talkerRoutes = express();
@@ -19,25 +22,61 @@ talkerRoutes.get('/', async (req, res) => {
   return res.status(200).json(getAllTalkerManagers);
 });
 
+// const caseOne = (arr, query) => {
+//   const { q, rate, date } = query;
+//   return arr.filter(({ name, talk: { rate: num, watchedAt } }) => (
+//     name.includes(q) && rate >= Number(num) && watchedAt === date));
+// };
+
+// const caseTwo = (arr, query) => {
+//   const { q, rate } = query;
+//   return arr.filter(({ name, talk: { rate: num } }) => (
+//     name.includes(q) && rate >= Number(num)
+//   ));
+// };
+
+// const caseThree = (arr, query) => (
+//   arr.filter(({ talk: { watchedAt } }) => watchedAt === query.date)
+// );
+
 talkerRoutes.get('/search',
   validateAuth,
+  validateDateQuery,
+  validateAllQueries,
+  validateQueries,
   validateRateQuery,
   async (req, res) => {
-  const { q, rate } = req.query;
-  const getAllTalkerManagers = await readFileTalker();
-  let getTalkerById = '';
-  if (q && rate) {
-    getTalkerById = getAllTalkerManagers.filter((talker) => talker.name.includes(q))
-      .filter((talker) => talker.talk.rate >= Number(rate));
-    res.status(200).json(getTalkerById);
-  } else if (!q && rate) {
-    getTalkerById = getAllTalkerManagers.filter((talker) => talker.talk.rate >= Number(rate));
-    res.status(200).json(getTalkerById);
-  } else {
-    getTalkerById = getAllTalkerManagers.filter((talker) => talker.name.includes(q));
-    return res.status(200).json(getTalkerById);
-  }
-});
+    const { q, rate } = req.query;
+    const getAllTalkerManagers = await readFileTalker();
+    let getTalkerById = '';
+    if (q && rate) {
+      getTalkerById = getAllTalkerManagers.filter((talker) => talker.name.includes(q))
+        .filter((talker) => talker.talk.rate >= Number(rate));
+      res.status(200).json(getTalkerById);
+    } else if (!q && rate) {
+      getTalkerById = getAllTalkerManagers.filter((talker) => talker.talk.rate >= Number(rate));
+      res.status(200).json(getTalkerById);
+    } else {
+      getTalkerById = getAllTalkerManagers.filter((talker) => talker.name.includes(q));
+      return res.status(200).json(getTalkerById);
+    }
+  });
+
+// // // // // // // // // /// // /// /// // // // // //// // // /// //
+// const { q, rate } = req.query;
+// const getAllTalkerManagers = await readFileTalker();
+// let getTalkerById = '';
+// if (q && rate) {
+//   getTalkerById = getAllTalkerManagers.filter((talker) => talker.name.includes(q))
+//     .filter((talker) => talker.talk.rate >= Number(rate));
+//   res.status(200).json(getTalkerById);
+// } else if (!q && rate) {
+//   getTalkerById = getAllTalkerManagers.filter((talker) => talker.talk.rate >= Number(rate));
+//   res.status(200).json(getTalkerById);
+// } else {
+//   getTalkerById = getAllTalkerManagers.filter((talker) => talker.name.includes(q));
+//   return res.status(200).json(getTalkerById);
+// }
 
 talkerRoutes.get('/:id', async (req, res) => {
   const { id } = req.params;
@@ -55,14 +94,14 @@ talkerRoutes.post('/',
   validateWatchAt,
   validateRate,
   async (req, res) => {
-  const getAllTalkers = await readFileTalker();
-  getAllTalkers.push({
-    id: getAllTalkers[getAllTalkers.length - 1].id + 1,
-    ...req.body,
+    const getAllTalkers = await readFileTalker();
+    getAllTalkers.push({
+      id: getAllTalkers[getAllTalkers.length - 1].id + 1,
+      ...req.body,
+    });
+    await writeFileTalker(getAllTalkers);
+    res.status(201).json(getAllTalkers[getAllTalkers.length - 1]);
   });
-  await writeFileTalker(getAllTalkers);
-  res.status(201).json(getAllTalkers[getAllTalkers.length - 1]);
-});
 
 talkerRoutes.put('/:id',
   validateAuth,
@@ -72,35 +111,35 @@ talkerRoutes.put('/:id',
   validateWatchAt,
   validateRate,
   async (req, res) => {
-  const { id } = req.params;
-  const getAllTalkerManagers = await readFileTalker();
-  let OK = 'no';
-  const getTalkerById = getAllTalkerManagers.map((talker) => {
-    if (talker.id === Number(id)) {
-      OK = 'yes';
-      return { ...talker, ...req.body, id: Number(id) };
+    const { id } = req.params;
+    const getAllTalkerManagers = await readFileTalker();
+    let OK = 'no';
+    const getTalkerById = getAllTalkerManagers.map((talker) => {
+      if (talker.id === Number(id)) {
+        OK = 'yes';
+        return { ...talker, ...req.body, id: Number(id) };
+      }
+      return talker;
+    });
+    if (OK === 'yes') {
+      await writeFileTalker(getTalkerById);
+      return res.status(200).json({ ...req.body, id: Number(id) });
     }
-    return talker;
+    return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
   });
-  if (OK === 'yes') {
-    await writeFileTalker(getTalkerById);
-    return res.status(200).json({ ...req.body, id: Number(id) });
-  }
-  return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
-});
 
 talkerRoutes.delete('/:id',
   validateAuth,
   async (req, res) => {
-  const { id } = req.params;
-  const getAllTalkerManagers = await readFileTalker();
-  const talkerDeleted = getAllTalkerManagers.findIndex((talker) => talker.id === Number(id));
-  if (talkerDeleted > -1) {
-    getAllTalkerManagers.splice(talkerDeleted, 1);
-    await writeFileTalker(getAllTalkerManagers);
-    return res.sendStatus(204);
-  }
-  return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
-});
+    const { id } = req.params;
+    const getAllTalkerManagers = await readFileTalker();
+    const talkerDeleted = getAllTalkerManagers.findIndex((talker) => talker.id === Number(id));
+    if (talkerDeleted > -1) {
+      getAllTalkerManagers.splice(talkerDeleted, 1);
+      await writeFileTalker(getAllTalkerManagers);
+      return res.sendStatus(204);
+    }
+    return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
+  });
 
 module.exports = talkerRoutes;
--- src/validations/validateTalker.js
+++ src/validations/validateTalker.js
@@ -1,3 +1,5 @@
+const { readFileTalker } = require('../indexTalker');
+
 const validateAuth = (req, res, next) => {
   const { authorization } = req.headers;
   if ('authorization' in req.headers) {
@@ -48,7 +50,7 @@ const validateRate = (req, res, next) => {
   if ('rate' in req.body.talk || 'rate' in req.query) {
     const valid = [rate > 0, rate < 6, Number.isInteger(rate)];
     return valid.every((i) => i === true) ? next()
-    : res.status(400).json({ message: 'O campo "rate" deve ser um número inteiro entre 1 e 5' });
+      : res.status(400).json({ message: 'O campo "rate" deve ser um número inteiro entre 1 e 5' });
   }
   return res.status(400).json(
     { message: 'O campo "rate" é obrigatório' },
@@ -61,11 +63,60 @@ const validateRateQuery = (req, res, next) => {
   if ('rate' in req.query) {
     const valid = [num > 0, num < 6, Number.isInteger(num)];
     return valid.every((i) => i === true) ? next()
-    : res.status(400).json({ message: 'O campo "rate" deve ser um número inteiro entre 1 e 5' });
+      : res.status(400).json({ message: 'O campo "rate" deve ser um número inteiro entre 1 e 5' });
   }
   return res.status(400).json(
     { message: 'O campo "rate" é obrigatório' },
   );
+};
+
+const validateDateQuery = async (req, res, next) => {
+  const { date } = req.query;
+  const talkers = await readFileTalker();
+  const regex = /^(\d{2})\/(\d{2})\/(\d{4})$/;
+  if (date === '') {
+    res.status(200).json(talkers);
+  } else if (!regex.test(date)) {
+    res.status(400).json({ message: 'O parâmetro "date" deve ter o formato "dd/mm/aaaa"' });
+  } else {
+    next();
+  }
+};
+
+const validateAllQueries = async (req, res, next) => {
+  const { date, q, rate } = req.query;
+  const valid = [['date', 'rate', 'q'], ['date', 'rate']];
+  const talkers = await readFileTalker();
+  let response = '';
+  if (valid[0].every((query) => query in req.query)) {
+    response = talkers.filter(({ name, talk: { rate: num, watchedAt } }) => (
+      date === watchedAt && num >= Number(rate) && name.includes(q)
+    ));
+    res.status(200).json(response);
+  } else if (valid[1].every((query) => query in req.query)) {
+    response = talkers.filter(({ talk: { rate: num, watchedAt } }) => (
+      date === watchedAt && num === Number(rate)
+    ));
+    res.status(200).json(response);
+  } else {
+    next();
+  }
+};
+
+const validateQueries = async (req, res, next) => {
+  const { date, q } = req.query;
+  const valid = [['date', 'q']];
+  const talkers = await readFileTalker();
+  if (valid[0].every((query) => query in req.query)) {
+    const response = talkers.filter(({ name, talk: { watchedAt } }) => (
+      date === watchedAt && name.includes(q)
+    ));
+    res.status(200).json(response);
+  } else if ('date' in req.query) {
+    res.status(200).json(talkers.filter(({ talk }) => talk.watchedAt === date));
+  } else {
+    next();
+  }
 };
 
 module.exports = {
@@ -76,4 +127,7 @@ module.exports = {
   validateWatchAt,
   validateRate,
   validateRateQuery,
+  validateDateQuery,
+  validateAllQueries,
+  validateQueries,
 };
